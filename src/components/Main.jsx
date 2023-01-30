4c64e6e55a4a14b51fdbdf752e1b2bbafb4cbfb2
amirhosein hasani <74850131+amirhoseinh73@users.noreply.github.com>
2023-01-30 13:04:19 +0330

    [FEAT]: complete click user and CPU

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,54 @@
-const Main = () => {
-  const state = {
-    id: null,
-    user: null,
-  };
-  const houses = Array(9).fill(state);
+import { useEffect, useState } from "react"
 
-  console.log(houses);
+const Main = () => {
+  const STATE = []
+  for (let i = 1; i <= 9; i++) {
+    STATE.push({
+      id: i,
+      user: null,
+    })
+  }
+
+  const [houses, setHouses] = useState(STATE)
+  const [clicked, setClicked] = useState(false)
+
+  const click = e => {
+    const id = Number(e.target.closest(".cell").dataset.id)
+    if (!id) return
+
+    const checkedHouse = houses.map(house => {
+      if (house.id === id && !house.user) house.user = 1
+      return house
+    })
+    setHouses(checkedHouse)
+    setClicked(true)
+  }
+
+  const randomNumber = () => Math.trunc(Math.random() * 9) + 1
+  let id = randomNumber()
+
+  useEffect(() => {
+    if (!clicked) return
+    const checkedHouse = houses.map(house => {
+      if (house.id === id) {
+        if (!house.user) {
+          house.user = 2
+          setClicked(false)
+        } else id = randomNumber()
+      }
+      return house
+    })
+    setHouses(checkedHouse)
+  }, [clicked, id])
 
   return (
     <article className="main">
       <div className="grid">
-        {houses.map((house, idx) => (
-          <section key={idx} className="cell">
+        {houses.map(house => (
+          <section key={house.id} data-id={house.id} className="cell">
             <button
               type="button"
+              onClick={click}
               className={
                 house.user &&
                 (house.user === 1
@@ -27,7 +62,7 @@ const Main = () => {
         ))}
       </div>
     </article>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
